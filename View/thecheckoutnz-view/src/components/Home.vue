e64da4dbe0ddc9925ae0243754dc6c7633099f46
rsef899 <rsef899@aucklanduni.ac.nz>
2023-06-26 01:42:54 +1200

    initial commit

---
+++ View/thecheckoutnz-view/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    Hello World
+
+    
+  </div>
+</template>
+
+<script>
+console.log("This is a test")
+
+
+export default {
+  name: 'HomeVue',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
