e1713c7ae83729e3fe26f290c13a8723d9bf1df4
rsef899 <rsef899@aucklanduni.ac.nz>
2023-06-29 18:53:49 +1200

    fetching all the categories

--- Backend/controller/NewWorldScraper/NewWorldScraper.js
+++ Backend/controller/NewWorldScraper/NewWorldScraper.js
@@ -10,25 +10,27 @@ let restrictedItemBoolean = false;
 let storeId = "c387ac97-5e0a-43ed-9c93-f1edccda298d";
 
 
-let url = "https://www.newworld.co.nz/next/api/products/search?";
-url += "category=" + category + "&";
-url += "storeId=" + storeId + "&";
-url += "ps=" + numItems + "&";
-url += "pg=" + pages + "&";
-url += "reg=" + restrictedItemBoolean;
 
-let config = {
-  method: 'get',
-  maxBodyLength: Infinity,
-  url: url,
-  headers: { 
-    'Cookie': '__cf_bm=mSagMqawJXFKGXyGlx3F.PjlC8JQXz.leHE1tODhuGM-1687867900-0-AYsGTN7Y0dvsC1RuU3cBYA79kKdgWyoz6+f6sOvtXQwq1LfNM+6vZpvUI1a+ZaiyoL25uJhHBp3b5HSu0SkUw6T8ZmnfVopd/yu+ep3F4W7L'
-  }
-};
 
-async function getallItems(){
+
+
+async function fetchAllCategories(){
+
+  //set the url to just get categories only store id is needed
+  let url = "https://www.newworld.co.nz/next/api/products/search?";
+  url += "storeId=" + storeId + "&";
+
+  let allCategoriesConfig = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: url,
+    headers: { 
+      'Cookie': '__cf_bm=mSagMqawJXFKGXyGlx3F.PjlC8JQXz.leHE1tODhuGM-1687867900-0-AYsGTN7Y0dvsC1RuU3cBYA79kKdgWyoz6+f6sOvtXQwq1LfNM+6vZpvUI1a+ZaiyoL25uJhHBp3b5HSu0SkUw6T8ZmnfVopd/yu+ep3F4W7L'
+    }
+  };
+
   try {
-    let response = await axios.request(config);
+    let response = await axios.request(allCategoriesConfig);
     let data = response.data;
     return data;
   } catch (error){
@@ -38,12 +40,54 @@ async function getallItems(){
 
 }
 
-async function main(){
+function getCategories(categoriesData){
+  let categories = [];
+  
+  try{
+    categories = Object.keys(categoriesData.data.facets.categories);
+    return categories;
+  }catch (error){
+    console.error(`ERROR: ${error}`)
+  }
+}
+
+async function getAllItems(){
+
+  let url = "https://www.newworld.co.nz/next/api/products/search?";
+  url += "category=" + category + "&";
+  url += "storeId=" + storeId + "&";
+  url += "ps=" + numItems + "&";
+  url += "pg=" + pages + "&";
+  url += "reg=" + restrictedItemBoolean;
+
+  let allItemsConfig = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: url,
+    headers: { 
+      'Cookie': '__cf_bm=mSagMqawJXFKGXyGlx3F.PjlC8JQXz.leHE1tODhuGM-1687867900-0-AYsGTN7Y0dvsC1RuU3cBYA79kKdgWyoz6+f6sOvtXQwq1LfNM+6vZpvUI1a+ZaiyoL25uJhHBp3b5HSu0SkUw6T8ZmnfVopd/yu+ep3F4W7L'
+    }
+  };
+
   try {
-    let productsArray = [];
+    let response = await axios.request(allItemsConfig);
+    let data = response.data;
+    return data;
+  } catch (error){
+    console.error(`ERROR: ${error}`);
+    throw error;
+  }
+}
+
+async function main(){
+  let categoryfetchData = await fetchAllCategories();
+  let categories = getCategories(categoryfetchData);
+  console.log(categories);
+  try {
+  let productsArray = [];
   let id = -1;
 
-  let allDataJSON = await getallItems();
+  let allDataJSON = await getAllItems();
   let productsFromSearch = allDataJSON.data.products;
   productsFromSearch.forEach(function(oneProduct){
     let productToPush = {};
@@ -60,7 +104,7 @@ async function main(){
 
   });
   
-  console.log(productsArray);
+  
   } catch (error){
     console.error(`ERROR: ${error}`)
   }
