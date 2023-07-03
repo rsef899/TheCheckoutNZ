6be2b2acf78ce8b9f18bdd35e333793c51390801
rsef899 <rsef899@aucklanduni.ac.nz>
2023-07-03 21:54:36 +1200

    initial commit, Initial Wrapper taken from project

--- Backend/controller/NewWorldScraper/NewWorldScraper.js
+++ Backend/controller/NewWorldScraper/NewWorldScraper.js
@@ -10,6 +10,40 @@ let storeId = "c387ac97-5e0a-43ed-9c93-f1edccda298d";
 
 let id = -1;
 
+async function fetchAllStores(){
+  let url = "https://www.newworld.co.nz/CommonApi/Store/GetStoreList";
+
+  let allStoresConfig = {
+    method: 'get',
+    maxBodyLength: 1024 * 1024 * 512, // Limit to 512MB
+    url: url
+  };
+
+  try {
+    let response = await axios.request(allStoresConfig);
+    let data = response.data;
+    return data;
+  } catch (error){
+    console.error(`ERROR: ${error}`);
+    throw error;
+  }
+
+}
+
+function getAllStores(storeData){
+  let stores = [];
+  storeData.stores.forEach(function(store, index){
+    stores[index] = {
+      name: store.name,
+      id: store.id,
+      address: store.address,
+      latitude: store.latitude,
+      longitude: store.longitude
+    }
+  });
+  return stores;
+}
+
 async function fetchAllCategories(){
 
   //set the url to just get categories only store id is needed
@@ -46,11 +80,11 @@ function getCategories(categoriesData){
  * @param {String} category 
  * @returns {Object}
  */
-async function fetchAllItemsOneCategory(category){
+async function fetchAllItemsOneCategory(category, store){
 
   let url = "https://www.newworld.co.nz/next/api/products/search?";
   url += "category=" + encodeURIComponent(category) + "&";
-  url += "storeId=" + storeId + "&";
+  url += "storeId=" + store + "&";
   url += "ps=" + numItems + "&";
   url += "pg=" + pages + "&";
   url += "reg=" + restrictedItemBoolean;
@@ -73,37 +107,48 @@ async function fetchAllItemsOneCategory(category){
 /**
  * @param {Array<String>} categories 
  */
-async function fetchAllitems(categories){
+async function fetchAllitems(categories, stores){
+  
   const MAX_RETRIES = 10;
   const REQUEST_DELAY_SEP = 150;
+  let itemsAllStores = {};
+
+
+  await Promise.all(
+    stores.map(async (store) =>{
+
+      let res = await Promise.all(categories.map(async (cat, index) => {
+        // Setup a delay between initial requests
+
+        await new Promise(resolve => setTimeout(resolve, index * REQUEST_DELAY_SEP));
+        //retry if request has failed
+        let retrySeparation = 1000;
+        for(let i = 0; i < MAX_RETRIES; i++) {
+          try {
+            console.log(`Sending request for items in category ${cat}`)
+            //return a promise
+            return await fetchAllItemsOneCategory(cat, store.id);
+          } catch {
+            console.warn(`Failed to fetch items for category ${cat}, retrying (attempt ${i + 2}/${MAX_RETRIES})`)
+          }
+          //Set timer for re-request if request failed, promise wait for promise to be resolved
+          await new Promise(resolve => setTimeout(resolve, retrySeparation));
+          //increase retry time
+          retrySeparation *= 2;
+        }
+      }));
+      //combine all products from all category searches into one array
+      //flat combines all into one array
+      let productsOneCategory = filterItems(res.flatMap(res => res.data.products.map(normaliseItem)));
+      itemsAllStores[store.id] = products;
+    }));
+    return itemsAllStores;
+
 
   //results is an array of resolved promises, and must await all promises set within it
-  let res = await Promise.all(categories.map(async (cat, index) => {
-    // Setup a delay between initial requests
-    await new Promise(resolve => setTimeout(resolve, index * REQUEST_DELAY_SEP));
 
-    //retry if request has failed
-    let retrySeparation = 1000;
-    for(let i = 0; i < MAX_RETRIES; i++) {
-      try {
-        console.log(`Sending request for items in category ${cat}`)
-        //return a promise
-        return await fetchAllItemsOneCategory(cat);
-      } catch {
-        console.warn(`Failed to fetch items for category ${cat}, retrying (attempt ${i + 2}/${MAX_RETRIES})`)
-      }
-
-      //Set timer for re-request if request failed, promise wait for promise to be resolved
-      await new Promise(resolve => setTimeout(resolve, retrySeparation));
-      //increase retry time
-      retrySeparation *= 2;
-    }
-  }));
-
-  //combine all products from all category searches into one array
-  //flat combines all into one array
-  return res.flatMap(res => res.data.products.map(normaliseItem));
 }
+
 
 
 function normaliseItem(item){
@@ -174,10 +219,11 @@ function writeToJson(allDataJSON){
 async function main(){
   let categoryfetchData = await fetchAllCategories();
   let categories = getCategories(categoryfetchData);
+  let storesFetch = await fetchAllStores();
+  let stores = getAllStores(storesFetch);
   try {
-    let allDataJSON = await fetchAllitems(categories);
-    let filteredDataJSON = filterItems(allDataJSON);
-    writeToJson(filteredDataJSON);
+    let allDataJSON = await fetchAllitems(categories, stores);
+    writeToJson(allDataJSON);
     
 
 
